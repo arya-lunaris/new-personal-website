d41144cfc1d082ff0c07b22914133d81cf1f2233
Arya Ram <arya_ram@outlook.com>
2025-10-01 00:49:25 +0100

    updated light toggle button style

--- src/components/LightDarkButton.jsx
+++ src/components/LightDarkButton.jsx
@@ -4,7 +4,11 @@ export default function LightDarkButton({ theme, toggleTheme }) {
     return (
         <button
             onClick={toggleTheme}
-            className="fixed top-4 right-4 z-50 px-1 py-0.5 sm:px-4 sm:py-2 rounded-full border border-gray-400 bg-white text-gray-800 hover:scale-105 transition-transform duration-300 cursor-pointer"
+            className={`fixed top-4 right-4 z-50 px-2 py-1 sm:px-4 sm:py-2 rounded-full border border-gray-400 hover:scale-105 transition-transform duration-300 cursor-pointer text-xs sm:text-base
+                ${theme === 'light'
+                    ? 'bg-white text-gray-800'
+                    : 'bg-[#1a1a1a] text-white'
+                }`}
             style={{ fontFamily: "'Fira Code', monospace" }}
         >
             {theme === 'light' ? (
@@ -20,7 +24,7 @@ export default function LightDarkButton({ theme, toggleTheme }) {
                 <>
                     Light
                     <img
-                        src="https://i.imgur.com/W2fiLn3.png"
+                        src="https://i.imgur.com/vU6L0bz.png"
                         alt="Sun icon"
                         className="inline w-6 h-6 ml-2 -mt-1"
                     />
