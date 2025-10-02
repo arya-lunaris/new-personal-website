f2c36311ad8f0b9f42304518023ec214a39f33de
Arya Ram <arya_ram@outlook.com>
2025-10-03 00:00:15 +0100

    added images locally for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,16 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar({ theme }) {
   return (
     <nav
-      className={`fixed top-0 left-0 h-screen w-35 md:w-65 shadow-[0_8px_30px_rgba(0,0,0,0.35)] flex flex-col items-center z-50 bg-cover bg-center ${theme === 'dark' ? 'bg-[#1a1a1a] text-white' : 'bg-[white] text-[#1D1D1F]'}`}
+      className={`fixed top-0 left-0 h-screen w-35 md:w-65 shadow-[0_8px_30px_rgba(0,0,0,0.35)] flex flex-col items-center z-50 bg-cover bg-center ${
+        theme === 'dark' ? 'bg-[#1a1a1a] text-white' : 'bg-[white] text-[#1D1D1F]'
+      }`}
     >
       <div className="mt-5">
         <a href="/">
           <img
-            src={theme === 'dark' ? "https://i.imgur.com/CklF5BM.png" : "https://imgur.com/T7E7ZTl.png"}
+            src={theme === 'dark' ? "/images/light-logo.png" : "/images/dark-logo.png"}
             alt="Logo"
-            className={`inline-block transform transition duration-300 hover:scale-110 ${
-              theme === 'dark' ? 'w-10 h-10' : 'w-10 h-10'
-
-            }`}
+            className="inline-block transform transition duration-300 hover:scale-110 w-10 h-10"
           />
         </a>
       </div>
@@ -43,13 +42,13 @@ export default function NavBar({ theme }) {
 
       <div className="mb-6 flex flex-row justify-center space-x-6">
         <a href="mailto:arya.ram.dev@outlook.com" className="flex items-center transform transition duration-500 hover:scale-110">
-          <img src={theme === 'dark' ? "https://i.imgur.com/krwISnr.png" : "https://imgur.com/XgkoBMa.png"} alt="Email icon" className="h-6 w-6 sm:h-7 sm:w-7" />
+          <img src={theme === 'dark' ? "/images/light-mail.png" : "/images/dark-mail.png"} alt="Email icon" className="h-6 w-6 sm:h-7 sm:w-7" />
         </a>
         <a href="https://linkedin.com/in/arya-ram-" className="flex items-center transform transition duration-500 hover:scale-110">
-          <img src={theme === 'dark' ? "https://i.imgur.com/H2eDhrp.png" : "https://i.imgur.com/LTVUBVS.png"} alt="LinkedIn icon" className="h-6 w-6 sm:h-7 sm:w-7" />
+          <img src={theme === 'dark' ? "/images/light-linkedin.png" : "/images/dark-linkedin.png"} alt="LinkedIn icon" className="h-6 w-6 sm:h-7 sm:w-7" />
         </a>
         <a href="https://github.com/arya-lunaris" className="flex items-center transform transition duration-500 hover:scale-110">
-          <img src={theme === 'dark' ? "https://i.imgur.com/FVTHHVH.png" : "https://i.imgur.com/VDLFjQj.png"} alt="GitHub icon" className="h-6 w-6 sm:h-7 sm:w-7" />
+          <img src={theme === 'dark' ? "/images/light-github.png" : "/images/dark-github.png"} alt="GitHub icon" className="h-6 w-6 sm:h-7 sm:w-7" />
         </a>
       </div>
     </nav>
