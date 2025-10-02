626e0fe1137fd3c6a422ba5c59cf6ba0f2071bec
Arya Ram <arya_ram@outlook.com>
2025-10-02 15:29:18 +0100

    added further effects for active navbar link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,6 @@ export default function NavBar({ theme }) {
       className={`fixed top-0 left-0 h-screen w-35 md:w-65 shadow-[0_8px_30px_rgba(0,0,0,0.35)] flex flex-col items-center z-50 bg-cover bg-center ${theme === 'dark' ? 'bg-[#1a1a1a] text-white' : 'bg-[white] text-[#1D1D1F]'}`}
     >
       <div className="mt-5">
-        {/* Logo */}
         <a href="/">
           <img
             src={theme === 'dark' ? "https://i.imgur.com/BZZuQuD.png" : "https://imgur.com/T7E7ZTl.png"}
@@ -17,7 +16,6 @@ export default function NavBar({ theme }) {
         </a>
       </div>
 
-      {/* Links */}
       <div className="flex-1 flex flex-col justify-center space-y-6 font-['Fira_Code'] text-center text-[17px]">
         {['/', '/about', '/projects', '/contact'].map((path) => {
           const label = path === '/' ? 'Home' : path.slice(1).charAt(0).toUpperCase() + path.slice(2);
@@ -26,10 +24,10 @@ export default function NavBar({ theme }) {
               <NavLink
                 to={path}
                 className={({ isActive }) =>
-                  `inline-block transform transition-transform duration-750 ${!isActive ? 'hover:translate-x-2' : ''} relative ${
+                  `inline-block transform transition-transform duration-750 relative ${
                     isActive
-                      ? 'font-medium before:content-[">"] before:absolute before:left-[-1.2rem] before:opacity-100 before:translate-x-0'
-                      : 'before:content-[">"] before:absolute before:left-[-1.2rem] before:opacity-0 before:-translate-x-[5px] before:transition-all before:duration-300'
+                      ? 'translate-x-2 font-medium before:content-[">"] before:absolute before:left-[-1.2rem] before:opacity-100 before:translate-x-0'
+                      : 'hover:translate-x-2 before:content-[">"] before:absolute before:left-[-1.2rem] before:opacity-0 before:-translate-x-[5px] before:transition-all before:duration-300'
                   }`
                 }
               >
@@ -40,7 +38,6 @@ export default function NavBar({ theme }) {
         })}
       </div>
 
-      {/* Social Icons */}
       <div className="mb-6 flex flex-row justify-center space-x-6">
         <a href="mailto:arya.ram.dev@outlook.com" className="flex items-center transform transition duration-500 hover:scale-110">
           <img src={theme === 'dark' ? "https://i.imgur.com/krwISnr.png" : "https://imgur.com/XgkoBMa.png"} alt="Email icon" className="h-6 w-6 sm:h-7 sm:w-7" />
